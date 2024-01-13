fde9686604354de54896f7996e1693657b0e5b6a
JamesZhang22 <jameszhangjunjie@gmail.com>
2024-01-13 17:26:55 -0500

    new nav

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -2,37 +2,17 @@ import React from "react";
 // We import bootstrap to make our application look better.
 import "bootstrap/dist/css/bootstrap.css";
  // We import NavLink to utilize the react router.
-import { NavLink } from "react-router-dom";
-import logo from '../images/logo.png';
+ import { FaDumbbell } from "react-icons/fa";
  // Here, we display our Navbar
 export default function Navbar() {
- return (
-   <div>
-     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-       <NavLink className="navbar-brand" to="/">
-       <img style={{"width" : 25 + '%'}} src={logo}></img>
-       </NavLink>
-       <button
-         className="navbar-toggler"
-         type="button"
-         data-toggle="collapse"
-         data-target="#navbarSupportedContent"
-         aria-controls="navbarSupportedContent"
-         aria-expanded="false"
-         aria-label="Toggle navigation"
-       >
-         <span className="navbar-toggler-icon"></span>
-       </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-         <ul className="navbar-nav ml-auto">
-           <li className="nav-item">
-             <NavLink className="nav-link" to="/create">
-               Create Record
-             </NavLink>
-           </li>
-         </ul>
-       </div>
-     </nav>
-   </div>
- );
+  return (
+    <div style={{display: "flex", justifyContent: "center", background: "#f1f1f1"}}>
+      <div style={{fontSize: "5vh", fontWeight: "bold", display: "flex", alignItems: "center"}}>
+        <FaDumbbell /> 
+        <div style={{padding: "0 8px"}}>
+          Exercise Tracker
+        </div>
+        <FaDumbbell /></div>
+    </div>
+  );
 }
